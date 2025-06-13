b5d6d7feb3f9a9a80aac39e3ca2da3a0977f14d6
Tirth Shah <10tirthshah@gmail.com>
2025-06-13 22:11:30 +0530

    0011-container-with-most-water 6 ms (7.13%) 56.8 MB (97.7%) Medium .java

---
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water-06-13-2025-22-11-30.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int max = 0;
+        while(i < j){
+            int area = (j-i)*Math.min(height[i], height[j]);
+            max = Math.max(max, area);
+            if(height[i] < height[j]) i++;
+            else if(height[i] > height[j]) j--;
+            else {
+                i++;
+                j--;
+            }
+        }
+        return max;
+    }
+}
